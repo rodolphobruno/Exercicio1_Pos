2391685ee5af9c75dca172b1dbb70acad09c8895
pos liberado <pospos@labcomp.usc>
2019-08-24 09:23:25 -0300

    Commit projeto

---
+++ src/app/dados/dados.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dados',
+  templateUrl: './dados.page.html',
+  styleUrls: ['./dados.page.scss'],
+})
+export class DadosPage implements OnInit {
+  private nome : string;
+  private idade : string;
+  private email : string;
+  private telefone : string;
+  //private copyright : string;
+
+  private pessoa : object;
+
+  constructor() {
+    this.nome = "Yoshi";
+    this.idade = "28";
+    this.email = "joao@supermario.com";
+    this.telefone =  "14 - 98865-4479"; 
+    
+    this.pessoa = {
+      nome : "Jailson",
+      idade : "39",
+      email : "jailso@msn.com",
+      telefone :  "11 - 98115-6355"
+    }
+
+
+  }
+
+
+
+  ngOnInit() {
+  }
+
+}
